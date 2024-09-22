3688d422a731774b49aaf71352ae30ab0faf32f1
Ponyu-dev <mopoypa@yandex.ru>
2024-09-22 11:12:06 +0200

    Update Constants.kt

--- core/src/main/java/com/ponyu/wather/core/Constants.kt
+++ core/src/main/java/com/ponyu/wather/core/Constants.kt
@@ -2,10 +2,8 @@ package com.ponyu.wather.core
 
 //TODO Переместить все эти константы по нужным местам.
 // Текст в текстовые ресурсы.
-// API_KEY получать из local.properties
 object NetworkService {
     const val BASE_URL: String = "https://api.openweathermap.org"
-    const val API_KEY: String = "162aeb696b8f9e04f02a40c6959493ea"
     const val UNITS: String = "metric"
     const val FORECAST_END_POINT = "/data/2.5/forecast"
 }
@@ -22,22 +20,4 @@ object Constants {
     const val FILL_FIELD = "Please fill in the field."
     const val DATA_NOT_FOUND = "No data available."
     const val UNKNOWN_HOST = "Unable to resolve host \"api.openweathermap.org\": No address associated with hostname"
-}
-
-object AppStrings {
-
-    // HomeScreen -> ForecastSection
-    const val hourly_forecast = "Hourly Forecast"
-    const val daily_forecast = "Daily Forecast"
-
-    // HomeScreen -> WeatherDetailSection
-    const val temp = "🌡 TEMP"
-    const val feels_like = "🌡 FEELS LIKE"
-    const val cloudiness = "☁ CLOUDINESS"
-    const val humidity = "💧 HUMIDITY"
-    const val sunrise = "🌇 SUNRISE"
-    const val sunset = "🌆 SUNSET"
-    const val wind = "🌬 WIND"
-    const val metric = "KM"
-    const val pressure = "⏲ PRESSURE"
 }
